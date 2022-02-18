6d4405a5084b792eda5b7cc484ed30458a0f37b0
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-02-18 15:30:52 +0600

    data pass use array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Card from './components/Card';
+import Data from './data.json';
 
 export default function App(){
+    // console.log(Data);
+    let items = [];
+    for(let x=0; x<Data.length; x++){
+        items.push(<Card headerText={Data[x].head} bodyText={Data[x].body} />);
+    }
+
     return <div>
+        <h1>Card Items</h1>
         <Card headerText="First Header" bodyText="First body ....." />
-        <Card headerText="Secong Header" bodyText="Secong body ....." />
+        {/* <Card headerText="Secong Header" bodyText="Secong body ....." /> */}
+        {items}
     </div>
 }
