929e77d83d84c0c2128a16c3fd8902237745eeea
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-02-18 14:51:16 +0600

    simple component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const variable_name = 'JS';
-ReactDOM.render(<h1>Hello React {variable_name}</h1>, document.getElementById('root'));
+
+ReactDOM.render(<Card />, document.getElementById('root'));
+
+function Card(){
+  return <>
+    <div className="card">
+      <div className="header">Hello head</div>
+      <div className="body">
+        Im Body
+        <h1>Hello React {variable_name}</h1>
+        </div>
+    </div>
+  </>
+}
