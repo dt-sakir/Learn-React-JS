ab01ec0f1d38891c0187cbd59e7d8cb0532af410
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-02-18 15:33:28 +0600

    data pass use mapping

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Data from './data.json';
 export default function App(){
     // console.log(Data);
     let items = [];
-    for(let x=0; x<Data.length; x++){
-        items.push(<Card headerText={Data[x].head} bodyText={Data[x].body} />);
-    }
+    // for(let x=0; x<Data.length; x++){
+    //     items.push(<Card headerText={Data[x].head} bodyText={Data[x].body} />);
+    // }
+
+    // items = Data.map((item) => <Card headerText={item.head} bodyText={item.body} />);
 
     return <div>
         <h1>Card Items</h1>
         <Card headerText="First Header" bodyText="First body ....." />
         {/* <Card headerText="Secong Header" bodyText="Secong body ....." /> */}
-        {items}
+        {/* {items} */}
+        {Data.map((item) => <Card headerText={item.head} bodyText={item.body} />)}
     </div>
 }
