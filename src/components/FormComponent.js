0960ed2bf3f2125e0bcd93362348863732aeb2dd
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-04-29 11:43:22 +0600

    Update FormComponent.js

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -4,20 +4,24 @@ export default class FormComponent extends Component {
     constructor(){
         super();
         this.state={
-            name:null
+            name:null,
+            email:null,
         }
     }
     onCHangeHandeler = (event) =>{
+        // state name get by input field name and set as value of this field
         this.setState({
-            name: event.target.value
-        })
+            [event.target.name] : event.target.value
+        });
     }
   render() {
     return (
       <div className='container m-5 border border-danger'>
           <form>
               <p>{this.state.name}</p>
-              <input type="text" className='form-control' placeholder='Your name' onChange={this.onCHangeHandeler} />
+              <input type="text" name='name' className='form-control' placeholder='Your name' onChange={this.onCHangeHandeler} />
+              <p>{this.state.email}</p>
+              <input type="email" name='email' className='form-control' placeholder='Your email' onChange={this.onCHangeHandeler} />
               <button className='btn btn-success text-white' type='submit'>SUBMIT</button>
           </form>
       </div>
