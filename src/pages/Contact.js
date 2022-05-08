2d4e1652898f26c031a10dae97951c1717ada2e7
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-08 18:01:47 +0600

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import { Form, Button } from 'react-bootstrap';
 
 export default class Contact extends Component {
     render() {
         return (
-            <Stack sx={{width: '100%'}} spacing={2}>
-                <Alert variant="filled" severity="info">
-                    Contact
-                </Alert>
-            </Stack>
+            <div className={'container'}>
+                <Stack sx={{width: '100%'}} spacing={2}>
+                    <Alert variant="filled" severity="info">
+                        Contact
+                    </Alert>
+                </Stack>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Note</Form.Label>
+                        <textarea className={'form-control'} placeholder="Note ......." />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+
         )
     }
 }
