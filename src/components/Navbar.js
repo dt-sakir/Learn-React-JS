5f3c4bb4b56b48d04b652c08b78fff9659dddef9
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-08 17:56:14 +0600

    slider

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default class Layout extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
+                        <Link className="navbar-brand" to="/">React App</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
