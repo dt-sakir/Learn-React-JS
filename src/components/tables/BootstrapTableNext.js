d42a24f0f8789d6648105928c6e26fe66f2c6ebc
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-09 21:11:59 +0600

    BootstrapTableNext

---
+++ src/components/tables/BootstrapTableNext.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const products = [ 
+    {'id': 1, 'name': 'P 1' }, {'id': 2, 'name': 'P 3' }, {'id': 3, 'name': 'P 3' }
+ ];
+const columns = [{
+  dataField: 'id',
+  text: 'Product ID'
+}, {
+  dataField: 'name',
+  text: 'Product Name'
+}];
+
+// export default () =>
+//   <BootstrapTable keyField='id' data={ products } columns={ columns } />
+  
+export default function BootstrapTableNext() {
+  return (
+    <BootstrapTable keyField='id' data={ products } columns={ columns } />
+
+  )
+}
--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,34 +1,14 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import Breadcrumb from "../components/Breadcrumb";
+import React, { Component } from 'react';
+import BootstrapTableNext from '../components/tables/BootstrapTableNext';
 
 class Table extends Component {
-    state = {
-        countries: []
-    }
-
-    componentDidMount() {
-        axios.get('https://restcountries.com/v3.1/all')
-            .then(response => {
-                // console.log(response.data);
-                this.setState({countries: response.data});
-            }).catch(error => {
-            console.log(error);
-            alert('Something went wrong');
-        })
-    }
-
     render() {
         return (
             <div>
-                <Breadcrumb title={'Table page'}/>
-                <ul>
-                    {
-                        this.state.countries.map(country =>
-                            <li>{country.name.common}</li>
-                        )
-                    }
-                </ul>
+                <h1>BootstrapTableNext</h1>
+                <BootstrapTableNext/>
+                <hr/>
+                
             </div>
         );
     }
