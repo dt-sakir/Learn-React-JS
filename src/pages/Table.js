a220fe9529aeb14bc85d6fa0ea9a6fda84be971d
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-09 21:48:19 +0600

    Update Table.js

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import BootstrapTableNext from '../components/tables/BootstrapTableNext';
+import Breadcrumb from "../components/Breadcrumb";
 
 class Table extends Component {
     render() {
         return (
             <div>
+                <Breadcrumb title={'Table page'}/>
                 <h1>BootstrapTableNext</h1>
                 <BootstrapTableNext/>
                 <hr/>
