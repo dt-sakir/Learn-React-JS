72ed404e557e0c377e83d3ab46a9ec4528e400f9
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-09 21:42:35 +0600

    Update BootstrapTableNext.js

--- src/components/tables/BootstrapTableNext.js
+++ src/components/tables/BootstrapTableNext.js
@@ -4,6 +4,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 const products = [ 
     {'id': 1, 'name': 'P 1' }, {'id': 2, 'name': 'P 3' }, {'id': 3, 'name': 'P 3' }
  ];
+ 
 const columns = [{
   dataField: 'id',
   text: 'Product ID'
@@ -17,7 +18,13 @@ const columns = [{
   
 export default function BootstrapTableNext() {
   return (
-    <BootstrapTable keyField='id' data={ products } columns={ columns } />
+    <BootstrapTable 
+    keyField='id' 
+    data={ products } 
+    columns={ columns } 
+    striped
+    hover
+    condensed/>
 
   )
 }
