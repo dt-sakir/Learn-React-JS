dd8c48b7483a474f43546161370d2cf5369d5e4b
Md. Sakir Ahmed <52326976+dt-sakir@users.noreply.github.com>
2022-05-09 17:21:24 +0600

    bootstrap nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,22 @@
 import React, {Component} from 'react'
 import {Outlet, Link} from "react-router-dom";
+import {Nav, Navbar, Container} from "react-bootstrap";
 
 export default class Layout extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">React App</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" to="/">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/blogs">Blogs</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/contact">Contact</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/table">Table</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/others">Others</Link>
-                                </li>
-                            </ul>
-                            <form className="d-flex">
-                                <input className="form-control me-2" type="search" placeholder="Search"
-                                       aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/">eGen</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                            <Nav.Link as={Link} to="contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="table">Table</Nav.Link>
+                            <Nav.Link as={Link} to="others">Others</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
                 <Outlet/>
             </div>
         )
